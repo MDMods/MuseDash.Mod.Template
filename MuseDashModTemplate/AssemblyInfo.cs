6f9486897e2b223d3906625e9d854fd9ce0089fa
lxymahatma <326265648@qq.com>
2024-02-07 23:05:53 +0800

    update template

--- MuseDashModTemplate/AssemblyInfo.cs
+++ MuseDashModTemplate/AssemblyInfo.cs
@@ -1,4 +1,7 @@
+using System.Reflection;
 using Main = MuseDashModTemplate.Main;
 
+[assembly: AssemblyVersion(ModVersion)]
+[assembly: AssemblyFileVersion(ModVersion)]
 [assembly: MelonInfo(typeof(Main), Name, ModVersion, Author)]
 [assembly: MelonGame("PeroPeroGames", "MuseDash")]
