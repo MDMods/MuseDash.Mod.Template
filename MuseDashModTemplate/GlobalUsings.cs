8b97c5e809ab2348b93909a25def19364cbbfa53
lxymahatma <326265648@qq.com>
2024-03-09 15:53:50 +0800

    update template

--- MuseDashModTemplate/GlobalUsings.cs
+++ MuseDashModTemplate/GlobalUsings.cs
@@ -1,11 +1,11 @@
 global using HarmonyLib;
 global using MelonLoader;
-global using static MuseDashModTemplate.MelonBuildInfo;
 #if UseMuseDashMirror
 global using MuseDashMirror.Attributes;
 global using MuseDashMirror.Attributes.EventAttributes.PatchEvents;
 global using MuseDashMirror.Attributes.EventAttributes.SceneEvents;
 global using MuseDashMirror.EventArguments;
+global using MuseDashMirror.Extensions;
 #endif
 #if UseUniTask
 global using Cysharp.Threading.Tasks;
@@ -13,4 +13,10 @@ global using Cysharp.Threading.Tasks.Linq;
 #endif
 #if UseZString
 global using Cysharp.Text;
+#endif
+global using static MuseDashModTemplate.MelonBuildInfo;
+#if UseMuseDashMirror
+global using static MuseDashMirror.UIComponents.Colors;
+global using static MuseDashMirror.UIComponents.Fonts;
+global using static MuseDashMirror.Utils.GameObjectUtils;
 #endif
