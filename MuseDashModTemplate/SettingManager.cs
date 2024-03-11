dc71172f561ecaad68426ae5ce318abb80f5db1e
lxymahatma <326265648@qq.com>
2024-03-11 12:16:02 +0800

    update template

--- MuseDashModTemplate/SettingManager.cs
+++ MuseDashModTemplate/SettingManager.cs
@@ -28,20 +28,25 @@ internal static class SettingManager
     }
 #endif
 #if MelonSetting
+    private static readonly string ConfigPath = Path.Combine("UserData", $"{Name}.cfg");
     private static MelonPreferences_Entry<bool> _exampleBool;
     private static MelonPreferences_Entry<int> _exampleInt;
     private static MelonPreferences_Entry<string> _exampleString;
+    private static MelonPreferences_Entry<Data> _exampleData;
     public static bool ExampleBool => _exampleBool.Value;
     public static int ExampleInt => _exampleInt.Value;
     public static string ExampleString => _exampleString.Value;
+    public static Data ExampleData => _exampleData.Value;
 
     internal static void Register()
     {
         var category = MelonPreferences.CreateCategory(Name, Name);
+        category.SetFilePath(ConfigPath);
 
         _exampleBool = category.CreateEntry("ExampleBool", false, "Example Bool");
         _exampleInt = category.CreateEntry("ExampleInt", 1, "Example Int");
         _exampleString = category.CreateEntry("ExampleString", "String", "Example String");
+        _exampleData = category.CreateEntry("ExampleData", new Data(), "Example Data");
     }
 #endif
 }
